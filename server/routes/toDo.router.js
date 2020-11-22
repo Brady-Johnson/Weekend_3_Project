7b47c202a7cf81df94c193e893be3da5af8e7a4f
BPJ94487 <BPJ94487@bethel.edu>
2020-11-22 15:43:59 -0600

    I have no idea how but my VS code auto made a new server code that was a copy from my koala project and put it in my file tree. It toally destroyed me becuase I did not realize I had two server files in my tree. Also the second file was not showing in finder.

--- server/routes/toDo.router.js
+++ server/routes/toDo.router.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const express = require('express');
-const { builtinModules } = require('module');
 const toDoRouter = express.Router();
 const pool = require('../modules/pool');
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const koalaRouter = require('../routes');
+const toDoRouter = require('./routes/toDo.router');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+
+app.use('/todo', toDoRouter);
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
